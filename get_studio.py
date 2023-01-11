4cfb3557ecddfdc1cdc76995135c6d74e45a293f
muse-sebeom <106875071+muse-sebeom@users.noreply.github.com>
2023-01-11 04:36:59 -0500

    Update get_studio.py

--- get_studio.py
+++ get_studio.py
@@ -3,7 +3,7 @@ import os
 
 
 if __name__ == "__main__":
-    mac_ver, _, chip  = platform.mac_ver()[0]
+    mac_ver, _, chip  = platform.mac_ver()
     mac_ver = mac_ver.split('.')[0]
     os.system(f'curl https://studio.muse.live/0.11.0/museLIVEStudio_osx{mac_ver}_{chip}_v0.11.0.dmg -o studio.dmg')
     print("Download Complete")
